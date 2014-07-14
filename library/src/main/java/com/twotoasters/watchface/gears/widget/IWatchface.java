23f1466d1934be47b8d8532152c12d68cabfd755
jbarr21 <james@twotoasters.com>
2014-07-14 08:37:49 -0400

    Initial commit

---
+++ library/src/main/java/com/twotoasters/watchface/gears/widget/IWatchface.java
@@ -0,0 +1,23 @@
+package com.twotoasters.watchface.gears.widget;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.os.Handler;
+
+import java.util.Calendar;
+
+public interface IWatchface {
+
+    // Implemented by the Watchface
+    public void onActiveStateChanged(boolean active);
+    public void onTimeChanged(Calendar time);
+
+    // Watchface should delegate these calls to the watch
+    public void onAttachedToWindow();
+    public void onDetachedFromWindow();
+
+    // Watchfaces should extend view and get these for free
+    public Context getContext();
+    public Handler getHandler();
+    public Resources getResources();
+}
